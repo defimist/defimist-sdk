1aaa0650a2b63741abaeab07a5538c0fb6e84e4f
defimist <help@defimist.com>
2021-06-26 07:39:42 +0200

    init

---
+++ src/constants.ts
@@ -0,0 +1,120 @@
+import JSBI from 'jsbi'
+import PERMISSIVE_MULTICALL_ABI from './abis/PermissiveMulticall.json'
+import STAKING_REWARDS_FACTORY_ABI from './abis/staking-rewards-distribution-factory.json'
+import STAKING_REWARDS_DISTRIBUTION_ABI from './abis/staking-rewards-distribution.json'
+import {
+  rinkeby as coreRinkeby,
+  mainnet as coreMainnet,
+  arbitrumTestnetV3 as coreArbitrumTestnetV3,
+  sokol as coreSokol,
+  dm as coreXDMT
+} from 'defimist-core/.contracts.json'
+import {
+  rinkeby as peripheryRinkeby,
+  mainnet as peripheryMainnet,
+  arbitrumTestnetV3 as peripheryArbitrumTestnetV3,
+  sokol as peripherySokol,
+  dm as peripheryXDMT
+} from 'defimist-periphery/.contracts.json'
+
+// exports for external consumption
+export type BigintIsh = JSBI | bigint | string
+
+export enum ChainId {
+  MAINNET = 1,
+  RINKEBY = 4,
+  ARBITRUM_TESTNET_V3 = 79377087078960,
+  SOKOL = 77,
+  DM = 100
+}
+
+export enum TradeType {
+  EXACT_INPUT,
+  EXACT_OUTPUT
+}
+
+export enum Rounding {
+  ROUND_DOWN,
+  ROUND_HALF_UP,
+  ROUND_UP
+}
+
+export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
+
+export const FACTORY_ADDRESS: { [chainId: number]: string } = {
+  [ChainId.MAINNET]: coreMainnet.factory,
+  [ChainId.RINKEBY]: coreRinkeby.factory,
+  [ChainId.ARBITRUM_TESTNET_V3]: coreArbitrumTestnetV3.factory,
+  [ChainId.SOKOL]: coreSokol.factory,
+  [ChainId.DM]: coreXDMT.factory
+}
+
+export const ROUTER_ADDRESS: { [chainId in ChainId]?: string } = {
+  [ChainId.RINKEBY]: peripheryRinkeby.router,
+  [ChainId.MAINNET]: peripheryMainnet.router,
+  [ChainId.ARBITRUM_TESTNET_V3]: peripheryArbitrumTestnetV3.router,
+  [ChainId.SOKOL]: peripherySokol.router,
+  [ChainId.DM]: peripheryXDMT.router
+}
+
+export const STAKING_REWARDS_FACTORY_ADDRESS: { [chainId: number]: string } = {
+  [ChainId.MAINNET]: '0x0000000000000000000000000000000000001234',
+  [ChainId.RINKEBY]: '0x163a3640Ce993A0b4c11885a6D4dAc16DFC188e1',
+  [ChainId.ARBITRUM_TESTNET_V3]: '0xB95Ad562EDE8DD78BBFC287fA18150e802b09D9F',
+  [ChainId.SOKOL]: '0xD436e756Cf41318ADeC62E8dCbEF2608753Ae068',
+  [ChainId.DM]: '0xCD2A45F36464FdB1065160e03A2353996Ea8Ff57'
+}
+
+export const DMSWAP_TOKEN_LIST_ID: { [chainId: number]: number } = {
+  [ChainId.MAINNET]: 1,
+  [ChainId.RINKEBY]: 1,
+  [ChainId.ARBITRUM_TESTNET_V3]: 1,
+  [ChainId.SOKOL]: 1,
+  [ChainId.DM]: 5
+}
+
+export const INIT_CODE_HASH = '0xd306a548755b9295ee49cc729e13ca4a45e00199bbd890fa146da43a50571776'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const TWO = JSBI.BigInt(2)
+export const THREE = JSBI.BigInt(3)
+export const FIVE = JSBI.BigInt(5)
+export const TEN = JSBI.BigInt(10)
+export const _25 = JSBI.BigInt(25)
+export const SECONDS_IN_YEAR = JSBI.BigInt(31536000)
+export const _30 = JSBI.BigInt(30)
+export const _100 = JSBI.BigInt(100)
+export const _1000 = JSBI.BigInt(1000)
+export const _10000 = JSBI.BigInt(10000)
+
+export const defaultSwapFee = _25
+export const defaultProtocolFeeDenominator = FIVE
+
+export enum SolidityType {
+  uint8 = 'uint8',
+  uint256 = 'uint256'
+}
+
+export const SOLIDITY_TYPE_MAXIMA = {
+  [SolidityType.uint8]: JSBI.BigInt('0xff'),
+  [SolidityType.uint256]: JSBI.BigInt('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+}
+
+const PERMISSIVE_MULTICALL_ADDRESS: { [chainId: number]: string } = {
+  [ChainId.MAINNET]: '0x0946f567d0ed891e6566c1da8e5093517f43571d',
+  [ChainId.RINKEBY]: '0x798d8ced4dff8f054a5153762187e84751a73344',
+  [ChainId.ARBITRUM_TESTNET_V3]: '0x73a08DC74eF4ed2c360199244bb69F1464204E7C',
+  [ChainId.SOKOL]: '0x4D97Bd8eFaCf46b33c4438Ed0B7B6AABfa2359FB',
+  [ChainId.DM]: '0x4E75068ED2338fCa56631E740B0723A6dbc1d5CD'
+}
+
+export {
+  PERMISSIVE_MULTICALL_ABI,
+  PERMISSIVE_MULTICALL_ADDRESS,
+  STAKING_REWARDS_FACTORY_ABI,
+  STAKING_REWARDS_DISTRIBUTION_ABI
+}
